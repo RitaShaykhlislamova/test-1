edf5789701da1c65dae5440bbbf3fba40a7ddf5a
RitaShaykhlislamova <rita.hrishkov@gmail.com>
2025-03-23 19:50:21 +0200

    Updates

--- src/Button.js
+++
@@ -1,9 +0,0 @@
-function Button(props) {
-    return (
-        <button style={props.color === 'red' ? {backgroundColor: "red"} : {backgroundColor:'blue'}}>
-            {props.text}
-
-        </button>
-    )
-}
-export default Button;
